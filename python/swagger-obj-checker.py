c83c5f787018d21bb0394a74412c7e5111ef2fba
Valerii Volkov <v.volkov@start.ru>
2023-04-07 18:23:34 +0400

    swagger-obj-checker

---
+++ python/swagger-obj-checker.py
@@ -0,0 +1,66 @@
+from typing import Any
+
+import yaml
+
+raw_schema = """
+ResponsePayonlineAuthFailProfileNotFound:
+    description: "Payment profile not found"
+    type: object
+    properties:
+      success:
+        type: boolean
+        example: false
+      code:
+        type: string
+        example: "PAYMENT_PROFILE_NOT_FOUND"
+      message:
+        type: string
+        example: "Не найден платежный профиль при заданных параметрах"
+      status_code:
+        type: integer
+        example: 400
+      data:
+        type: object
+        properties:
+          currency:
+            type: string
+            example: "USD"
+"""
+
+
+def check_object_shema(obj: Any, schema: dict, schema_path: str):
+    schema_type = schema.get("type")
+    assert schema_type is not None, f"{schema_path} schema not defined"
+    if schema_type == "object":
+        assert isinstance(obj, dict), f"{schema_path} is not object({obj}: {type(obj)}"
+        schema_obj_properties = schema.get("properties") or {}
+        fields = set([*schema_obj_properties.keys(), *obj.keys()])
+        assert len(fields) > 0, f"{schema_path} has not fields?"
+        for field in fields:
+            assert field in obj, f"{schema_path} response has no {field}"
+            assert field in schema_obj_properties, f"{schema_path} schema has no {field}"
+            check_object_shema(obj[field], schema_obj_properties[field], f"{schema_path}.{field}")
+    elif schema_type == "string":
+        assert isinstance(obj, str), f"{schema_path} response is not str"
+    elif schema_type == "boolean":
+        assert isinstance(obj, bool), f"{schema_path} response is not bool"
+    elif schema_type == "integer":
+        assert isinstance(obj, int), f"{schema_path} response is not int"
+    else:
+        NotImplementedError(schema_type)
+
+
+obj_schema = yaml.load(raw_schema, yaml.SafeLoader)
+obj = {
+    "success": True,
+    "code": "SOME_CODE",
+    "message": "Some messages",
+    "status_code": 1223,
+    "data": {"currency": "ru" }
+}
+
+check_object_shema(
+    obj,
+    obj_schema["ResponsePayonlineAuthFailProfileNotFound"],
+    "payonline_auth.response.200"
+)
